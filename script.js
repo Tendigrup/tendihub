15a22ef93375f45dd7a15e39245adc7cb4364a7c
tendihub <denissahanya@gmail.com>
2024-11-19 12:31:01 +0700

    Memperbarui index.html, script.js, dan styles.css dengan fitur baru

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('TendiHub is ready!');
+// Contoh script sederhana untuk animasi pada tombol
+document.querySelector('.btn').addEventListener('mouseover', function() {
+    this.style.backgroundColor = '#e65c00';
+});
+
+document.querySelector('.btn').addEventListener('mouseout', function() {
+    this.style.backgroundColor = '#ff6600';
+});
